92507dd9706381d71f68fd9750cdefa6f27f25f6
Crowniie <feernarosso@gmail.com>
2026-05-26 19:55:46 +0200

    Completed main structure

--- Backend/main.py
+++ Backend/main.py
@@ -43,11 +43,59 @@ class ResponseStructure(BaseModel):
     uid:str = Field(..., description="Unique identifier for the user", example="user_001")
     timestamp:str = Field(..., description="Time of the response in ISO format", example="2026-01-01T00:00:00Z")
     signals:List[TickerResponse] = Field(..., description="List of trading signals for the tickers")
+    
+    
+#------------------------API Endpoints------------------------
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    """Healthcheck basico."""
+    return {"status": "ok", "service": "investment-advisor-backend"}
 
-@app.get("/n8n/Endpoint/Placeholder")
-def process_n8n_data():
-    # Placeholder for processing data from n8n
-    return {"message": "Data processed successfully"}
+@app.post("/analyze", response_model=ResponseStructure)
+def analyze_ticker_data(request: RequestProcessing)->ResponseStructure:
+    """Analyyzes the provided ticker data and returns the signals
+    Expects body:
+        {
+          "user_id": "user_001",
+          "tickers_data": {
+            "AAPL": {"bars": [{"t": "...", "o": ..., "h": ..., "l": ..., "c": ..., "v": ...}, ...]},....
+    }
+    Returns body:
+        {
+          "user_id": "user_001",
+          "timestamp": "2026-01-01T00:00:00Z",
+          "signals": [
+            {
+              "ticker": "AAPL",
+              "signal": "buy",
+              "reason": "The price cut the MA(200) above, indicating a potential trend upwards.",
+              "price": 110.0
+            },...
+    Raises:
+        -HtttpException 400: If the input data is invalid or if the analysis fails.
+    """
+    if not request.data:
+        raise HTTPException(status_code=400, detail="Error, no ticker data provided")
+    #Initialize the response structure
+    signals = List[TickerData] = []
+    #Process each ticker data
+    for ticker, ticker_data in request.data.items():
+    #In case a specific ticker dosent have data, we will return a hold singnal to avoid botching remaining tickers if there is no specific data just for one ticker
+        if not ticker_data.data:
+            signals.append(
+                TickerResponse(
+                    ticker=ticker,
+                    signal="HOLD",
+                    reason="No hay datos disponibles",
+                    price=0.0,
+                ))
+        continue
+    data_list = [data.model_dump() for data in ticker_data.data]
+    result = analyze_ticker(ticker, data_list)
+    signals.append(TickerResponse(**result))
+    return ResponseStructure(
+        uid=request.uid,
+        timestamp=datetime.now(timezone.utc).isoformat(),
+        signals=signals,
+    )
+    

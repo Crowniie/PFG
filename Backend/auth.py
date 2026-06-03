c65a23bfe6b74fcb950ac3f70bf23d400d519200
Crowniie <feernarosso@gmail.com>
2026-06-03 14:46:39 +0200

    Completed Auth logic pending new endpoint

--- Backend/auth.py
+++ Backend/auth.py
@@ -1,6 +1,29 @@
 import os
-import bycrypt
+import bcrypt
 import httpx
 from typing import Optional
 from pydantic import HTTPException
-from fastapi import BaseMModel, EmailStr
+from fastapi import BaseModel, EmailStr
+
+#------------------------------Models for athentication------------------------------
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class LoginResponse(BaseModel):
+    success: bool
+    
+#------------------------------Authentication function------------------------------
+async def verify_password(submitted_password: str, stored_password: str) -> LoginResponse:
+    if not submitted_password or not stored_password:
+        return False
+    try:
+        return(
+            bcrypt.checkpw(
+                submitted_password.encode("utf-8"),
+                stored_password.encode("utf-8")
+            )
+        )
+    except(ValueError, TypeError):
+        return False

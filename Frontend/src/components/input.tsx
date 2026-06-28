380dd6b424f489df92669ecea208890c24692086
Crowniie <feernarosso@gmail.com>
2026-06-28 14:26:59 +0200

    re implemented forward ref correctly

--- Frontend/src/components/input.tsx
+++ Frontend/src/components/input.tsx
@@ -6,19 +6,38 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   hasIconRight?: boolean;
 }
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className = "", hasIconLeft, hasIconRight, ...rest }, ref) => {
-    const paddingLeft = hasIconLeft ? "pl-11" : "pl-3";
-    const paddingRight = hasIconRight ? "pr-11" : "pr-3";
-    return (
-      <input
-        {...rest}
-        ref={ref}
-        className={`w-full bg-slate-950 border border-slate-700 text-slate-100 rounded-lg py-2.5 ${paddingLeft} ${paddingRight} focus:outline-none focus:border-teal-400 focus:ring-1 focus:ring-teal-400 transition-colors placeholder:text-slate-600 ${className}`}
-      />
-    );
-  }
-);
+// uso forwardRef porque algunos sitios le pasan una ref al input
+const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
+  props,
+  ref
+) {
+  const { className = "", hasIconLeft, hasIconRight, ...rest } = props;
 
-Input.displayName = "Input";
+  // si hay icono a la izquierda le dejo mas hueco
+  let paddingLeft = "pl-3";
+  if (hasIconLeft) {
+    paddingLeft = "pl-11";
+  }
+
+  let paddingRight = "pr-3";
+  if (hasIconRight) {
+    paddingRight = "pr-11";
+  }
+
+  return (
+    <input
+      {...rest}
+      ref={ref}
+      className={
+        "w-full bg-slate-950 border border-slate-700 text-slate-100 rounded-lg py-2.5 " +
+        paddingLeft +
+        " " +
+        paddingRight +
+        " focus:outline-none focus:border-teal-400 focus:ring-1 focus:ring-teal-400 transition-colors placeholder:text-slate-600 " +
+        className
+      }
+    />
+  );
+});
+
 export default Input;
